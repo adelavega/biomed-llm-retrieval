504b01110599528c5291937a1013b6fc2e8a7b2b
Alejandro de la Vega <aleph4@gmail.com>
2023-08-09 18:33:03 -0500

    Update template and re-run

--- templates.py
+++ templates.py
@@ -4,9 +4,9 @@ ZERO_SHOT_SINGLE_GROUP_DEMOGRAPHICS = {
                 The sample is delimited with triple backticks.
 
                 Perform the following tasks:
-                1. Identify the total number of participants in the fMRI or neuroimaging study, if any. Do not include the number of participants in the behavioral study. Do not include the age of the participants.
+                1. Identify the total number of participants that underwent fMRI or neuroimaging in the study, if any. 
                 2. Provide your response in a JSON format containing a single key `count` and a integer value corresponding to the number of participants. 
-                Do not provide any additional information except the JSON. If the number of participants is not mentioned in the text, provide `n/a` as the value, instead of 0.
+                Do not provide any additional information except the JSON. If the number of participants is not mentioned in the text, provide `n/a` as the value. If the number of participants is 0 return `n/a`.
 
                 Text sample: ```{text}```
 
