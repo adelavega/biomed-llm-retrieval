df9e85d659cd2d33885fd370cd98f8af8145c857
Alejandro de la Vega <aleph4@gmail.com>
2024-04-04 21:23:22 -0500

    Reorganize workflow

--- extract_demographics_gpt.py
+++ extract_demographics_gpt.py
@@ -1,18 +1,25 @@
 import pandas as pd
+import json
 from pathlib import Path
-from publang_pipelines.demographics import extract_gpt_demographics, clean_gpt_demo_predictions
-from labelrepo.projects.participant_demographics import get_participant_demographics
+from publang.pipelines import search_extract, extract_from_text
+from nipub_templates.demographics import (
+    clean_gpt_demo_predictions, ZERO_SHOT_MULTI_GROUP)
+from publang.utils.split import split_pmc_document
+from labelrepo.projects.participant_demographics import \
+        get_participant_demographics
 from labelrepo import database
+from tqdm import tqdm
 
-
-### Training documents
 # Load original annotations
 subgroups = get_participant_demographics()
-jerome_pd = subgroups[(subgroups.project_name == 'participant_demographics') & \
-                      (subgroups.annotator_name == 'Jerome_Dockes')]
-subset_cols = ['count', 'diagnosis', 'group_name', 'subgroup_name', 'male count',
-       'female count', 'age mean', 'age minimum', 'age maximum',
-       'age median', 'pmcid']
+jerome_pd = subgroups[
+    (subgroups.project_name == 'participant_demographics') &
+    (subgroups.annotator_name == 'Jerome_Dockes')
+    ]
+subset_cols = [
+    'count', 'diagnosis', 'group_name', 'subgroup_name', 'male count',
+    'female count', 'age mean', 'age minimum', 'age maximum',
+    'age median', 'pmcid']
 jerome_pd_subset = jerome_pd[subset_cols].sort_values('pmcid')
 
 # database.make_database()
@@ -24,23 +31,55 @@ docs = pd.read_sql(
 )
 docs = docs[docs.pmcid.isin(jerome_pd.pmcid)].to_dict(orient='records')
 
-model_name = 'gpt-3.5-turbo-1106'
+model_name = 'gpt-4-0125-preview'
 
 output_dir = Path('outputs')
 
-docs = docs[0:2]
+# for max_chars in [4000, 20000]:
+#     predictions_path = output_dir / \
+#         f'eval_participant_demographics_{model_name}_tokens-{max_chars}.json'
+#     clean_predictions_path = output_dir / \
+#         f'eval_participant_demographics_{model_name}_tokens-{max_chars}_clean.csv'
+#     embeddings_path = output_dir / \
+#         f'eval_embeddings_tokens-{max_chars}.parquet'
 
-for max_chars in [10000]:
+#     # Extract
+#     predictions = search_extract(
+#         articles=docs, output_path=predictions_path, max_chars=max_chars,
+#         embeddings_path=embeddings_path, extraction_model_name=model_name,
+#         num_workers=6, **ZERO_SHOT_MULTI_GROUP
+#     )
 
-    predictions_path = output_dir / f'eval_participant_demographics_{model_name}_tokens-{max_chars}.csv'
-    clean_predictions_path = output_dir / f'eval_participant_demographics_{model_name}_tokens-{max_chars}_clean.csv'
-    embeddings_path = output_dir / f'eval_embeddings_tokens-{max_chars}.parquet'
+#     clean_gpt_demo_predictions(predictions).to_csv(
+#         clean_predictions_path, index=False)
 
-    # Extract
-    predictions = extract_gpt_demographics(
-        articles=docs, output_path=predictions_path, max_chars=max_chars, num_workers=1,
-        embeddings_path=embeddings_path, extraction_model_name=model_name
+
+# Extract using full Body
+predictions_path = output_dir / \
+    f'eval_participant_demographics_{model_name}_tokens-all_body.json'
+clean_predictions_path = output_dir / \
+    f'eval_participant_demographics_{model_name}_tokens-all_body_clean.csv'
+
+# Get text body
+all_preds = []
+for doc in tqdm(docs):
+    split_doc = split_pmc_document(doc['text'], delimiters=['# '])
+    if split_doc is None:
+        continue
+
+    text = [section['content'] for section in split_doc
+            if section.get('section_0') == 'Body'][0]
+
+    prediction = extract_from_text(
+        text, model_name=model_name,
+        num_workers=6, messages=ZERO_SHOT_MULTI_GROUP['messages'],
+        output_schema=ZERO_SHOT_MULTI_GROUP['output_schema']
     )
 
-    clean_gpt_demo_predictions(predictions).to_csv(clean_predictions_path, index=False)
-    predictions.to_csv(predictions_path, index=False)
+    prediction['pmcid'] = doc['pmcid']
+    all_preds.append(prediction)
+
+json.dump(all_preds, open(predictions_path, 'w'))
+
+clean_gpt_demo_predictions(all_preds).to_csv(
+    clean_predictions_path, index=False)
---
+++ nipub_templates/demographics.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import numpy as np
+
+ZERO_SHOT_MULTI_GROUP = {
+    "search_query": "How many participants or subjects were recruited for this study?",
+    "messages": [
+        {
+            "role": "user",
+            "content": """You will be provided with a text sample from a scientific journal. 
+                The sample is delimited with triple backticks.
+                        
+                Your task is to identify groups of participants that participated in the study, and underwent MRI.
+                If there is no mention of any participant groups, return a null array.
+                
+                For each group identify:
+                   - the number of participants in each group, and the diagnosis. 
+                   - the number of male participants, and their mean age, median age, minimum and maximum age
+                   - the number of female participants, and their mean age, median age, minimum and maximum age.
+                   - 
+                If any of the information is missing, return `null` for that field.               
+                
+                Call the extractData function to save the output.
+
+                Text sample: ```{text}```
+                """,
+        }
+    ],
+    "output_schema": {
+        "type": "object",
+        "properties": {
+            "groups": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "count": {
+                            "description": "Number of participants in this group",
+                            "type": "integer",
+                        },
+                        "diagnosis": {
+                            "description": "Diagnosis of the group, if any",
+                            "type": "string",
+                        },
+                        "group_name": {
+                            "description": "Group name, healthy or patients",
+                            "type": "string",
+                            "enum": ["healthy", "patients"],
+                        },
+                        "subgroup_name": {
+                            "description": "Subgroup name",
+                            "type": "string",
+                        },
+                        "male count": {
+                            "description": "Number of male participants in this group",
+                            "type": "integer",
+                        },
+                        "female count": {
+                            "description": "Number of female participants in this group",
+                            "type": "integer",
+                        },
+                        "age mean": {
+                            "description": "Mean age of participants in this group",
+                            "type": "number",
+                        },
+                        "age range": {
+                            "description": "Age range of participants in this group, separated by a dash",
+                            "type": "string",
+                        },
+                        "age minimum": {
+                            "description": "Minimum age of participants in this group",
+                            "type": "integer",
+                        },
+                        "age maximum": {
+                            "description": "Maximum age of participants in this group",
+                            "type": "integer",
+                        },
+                        "age median": {
+                            "description": "Median age of participants in this group",
+                            "type": "integer",
+                        },
+                    },
+                    "required": ["count"],
+                },
+            }
+        },
+    },
+}
+
+
+def clean_gpt_demo_predictions(predictions):
+    # Clean known issues with GPT demographics predictions
+    predictions = [p for p in predictions if "groups" in p]
+
+    meta_keys = ["pmcid", "rank", "start_char", "end_char"]
+    meta_keys = [k for k in meta_keys if k in predictions[0]]
+    
+    # Convert JSON to DataFrame
+    predictions = pd.json_normalize(
+        predictions, record_path=["groups"],
+        meta=meta_keys
+        )
+
+    predictions = predictions.fillna(value=np.nan)
+    predictions["group_name"] = predictions["group_name"].fillna("healthy")
+
+    # If group name is healthy, blank out diagnosis
+    predictions.loc[predictions.group_name == "healthy", "diagnosis"] = np.nan
+    predictions = predictions.replace(0.0, np.nan)
+
+    # Drop rows where count is NA
+    predictions = predictions[~pd.isna(predictions["count"])]
+
+    # Set group_name to healthy if no diagnosis
+    predictions.loc[
+        (predictions["group_name"] != "healthy") & (pd.isna(predictions["diagnosis"])),
+        "group_name",
+    ] = "healthy"
+
+    # If no male count, substract count from female count columns
+    ix_male_miss = (pd.isna(predictions["male count"])) & ~(
+        pd.isna(predictions["female count"])
+    )
+    predictions.loc[ix_male_miss, "male count"] = (
+        predictions.loc[ix_male_miss, "count"]
+        - predictions.loc[ix_male_miss, "female count"]
+    )
+
+    # Same for female count
+    ix_female_miss = (pd.isna(predictions["female count"])) & ~(
+        pd.isna(predictions["male count"])
+    )
+    predictions.loc[ix_female_miss, "female count"] = (
+        predictions.loc[ix_female_miss, "count"]
+        - predictions.loc[ix_female_miss, "male count"]
+    )
+
+    return predictions
