a202cf03736403a8724f2909b94922ae6b7bf486
Alejandro de la Vega <aleph4@gmail.com>
2024-05-22 16:39:17 -0500

    Re-organize

---
+++ scripts/extract_demographics.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+from pathlib import Path
+from nipub_templates.prompts import FEW_SHOT_FC, ZERO_SHOT_MULTI_GROUP_FC, FEW_SHOT_FC_2
+from nipub_templates.clean import clean_predictions
+from publang.pipelines import search_extract
+from labelrepo.projects.participant_demographics import \
+        get_participant_demographics
+from labelrepo import database
+from openai import OpenAI
+
+# Load original annotations
+combined_annotations = pd.read_csv('../annotations/combined_pd.csv')
+
+# TMP: ONLY EXTRACT NEW ANNOTATIONS
+combined_annotations = combined_annotations[
+    combined_annotations.annotator_name != 'Jerome_Dockes']
+
+# Load articles that have been annotated
+docs = pd.read_sql(
+    "select pmcid, text from document",
+    database.get_database_connection(),
+)
+docs = docs[
+    docs.pmcid.isin(combined_annotations.pmcid)].to_dict(orient='records')
+
+output_dir = Path('../outputs')
+
+# Set up OpenAI clients
+embed_model = 'text-embedding-ada-002'
+openai_client = OpenAI(api_key=os.getenv('MYOPENAI_API_KEY'))
+fireworks_client = OpenAI(api_key=os.getenv('FIREWORKS_API_KEY'), 
+                          base_url='https://api.fireworks.ai/inference/v1')
+
+
+def _run(extraction_model, extraction_client, min_chars, max_chars,
+         prepend='', **extract_kwargs):
+    prepend += '_'
+    short_model_name = extraction_model.split('/')[-1]
+
+    embeddings_path = output_dir / \
+        f'eval_embeddings_minc-{min_chars}_maxc-{max_chars}.parquet'
+
+    name = f"eval_{prepend}{short_model_name}_minc-{min_chars}_maxc-{max_chars}"
+    predictions_path = output_dir / f'{name}.json'
+    clean_predictions_path = output_dir / f'{name}_clean.csv'
+
+    # Extract
+    predictions = search_extract(
+        articles=docs, output_path=predictions_path,
+        min_chars=min_chars, max_chars=max_chars,
+        embeds_path=embeddings_path, extraction_model=extraction_model,
+        embed_model=embed_model, embed_client=openai_client,
+        extraction_client=extraction_client,
+        **extract_kwargs
+    )
+
+    clean_predictions(predictions).to_csv(
+        clean_predictions_path, index=False
+    )
+
+
+models = [
+    ("accounts/fireworks/models/firefunction-v1", fireworks_client),
+    # ("gpt-3.5-turbo-0613", openai_client),
+    # ("gpt-4-0125-preview", openai_client),
+    # ("gpt-4o-2024-05-13", openai_client),
+]
+
+
+# Split body into large sections (by setting min_chars to high number)
+for model_name, client in models:
+    _run(model_name, client, 40, 4000, prepend='demographics-fewshot',
+         **FEW_SHOT_FC, num_workers=10)
--- scripts/extract_demographics_all.py
+++ scripts/extract_demographics_all.py
@@ -1,30 +1,14 @@
 import pandas as pd
 import os
 from pathlib import Path
-from nipub_templates.prompts import FEW_SHOT_FC, ZERO_SHOT_MULTI_GROUP_FC, FEW_SHOT_FC_2
+from nipub_templates.prompts import ZERO_SHOT_MULTI_GROUP_FC
 from nipub_templates.clean import clean_predictions
 from publang.pipelines import search_extract
-from labelrepo.projects.participant_demographics import \
-        get_participant_demographics
-from labelrepo import database
 from openai import OpenAI
 
-# Load original annotations
-combined_annotations = pd.read_csv('annotations/combined_pd.csv')
 
-# TMP: ONLY EXTRACT NEW ANNOTATIONS
-combined_annotations = combined_annotations[
-    combined_annotations.annotator_name != 'Jerome_Dockes']
 
-# Load articles that have been annotated
-docs = pd.read_sql(
-    "select pmcid, text from document",
-    database.get_database_connection(),
-)
-docs = docs[
-    docs.pmcid.isin(combined_annotations.pmcid)].to_dict(orient='records')
-
-output_dir = Path('outputs')
+output_dir = Path('../outputs')
 
 # Set up OpenAI clients
 embed_model = 'text-embedding-ada-002'
@@ -39,9 +23,9 @@ def _run(extraction_model, extraction_client, min_chars, max_chars,
     short_model_name = extraction_model.split('/')[-1]
 
     embeddings_path = output_dir / \
-        f'eval_embeddings_minc-{min_chars}_maxc-{max_chars}.parquet'
+        f'all_embeddings_minc-{min_chars}_maxc-{max_chars}.parquet'
 
-    name = f"eval_{prepend}{short_model_name}_minc-{min_chars}_maxc-{max_chars}"
+    name = f"all_{prepend}{short_model_name}_minc-{min_chars}_maxc-{max_chars}"
     predictions_path = output_dir / f'{name}.json'
     clean_predictions_path = output_dir / f'{name}_clean.csv'
 
@@ -62,13 +46,11 @@ def _run(extraction_model, extraction_client, min_chars, max_chars,
 
 models = [
     # ("accounts/fireworks/models/firefunction-v1", fireworks_client),
-    ("gpt-3.5-turbo-0613", openai_client),
-    ("gpt-4-0125-preview", openai_client),
-    # ("gpt-4o-2024-05-13", openai_client),
+    ("gpt-4o-2024-05-13", openai_client),
 ]
 
 
 # Split body into large sections (by setting min_chars to high number)
 for model_name, client in models:
-    _run(model_name, client, 40, 4000, prepend='demographics-fewshot',
-         **FEW_SHOT_FC, num_workers=10)
+    _run(model_name, client, 40, 4000, prepend='demographics-zeroshot',
+         **ZERO_SHOT_MULTI_GROUP_FC, num_workers=10)
